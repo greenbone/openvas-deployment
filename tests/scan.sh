14440333f1e29848c10e503926e75eb84c24a186
Pascal Holthaus <pascal.holthaus@greenbone.net>
2026-08-14 12:42:52 +0200

    Add: test openvasd to gvmd

--- tests/scan.sh
+++ tests/scan.sh
@@ -30,14 +30,14 @@ check_req() {
 }
 
 clean() {
-    echo 'Show docker container'
+    echo_task 'Show docker container for $1'
     docker ps
-    echo 'Test down volumes'
+    echo_task 'Test down volumes'
     openvas-deployment --down-volumes
 }
 
 list() {
-    echo 'List product folder'
+    echo_task 'List product folder for $1'
     tree product
 }
 
@@ -46,8 +46,10 @@ gvmd_add_openvasd_host_to_etc_hosts() {
     local ip="${2:?Missing IP argument}"
 
     docker exec -u 0 enterprise-container-scan-gvmd-1 sh -c "chmod 0666 /etc/hosts && echo \"$ip $domain\" >> /etc/hosts"
-    #sudo chmod 0666 /etc/hosts
-    #echo "$ip $domain" >> /etc/hosts
+    if [ "$MHOSTS" ]; then
+        sudo chmod 0666 /etc/hosts
+        echo "$ip $domain" >> /etc/hosts
+    fi
 }
 
 run() {
@@ -65,7 +67,7 @@ run_openvasd_cert_tar() {
     echo_task 'Test Openvasd cert tar'
     openvas-deployment --create-openvasd-cert-tar --cn-openvasd sensor1.test.test
     if ! [ -f 'sensor1-test-test.tar' ]; then
-        echo_error 'Openvasd cert tar gen failed!'
+        echo_error 'Error openvasd cert tar gen failed!'
         exit 1
     fi
     mkdir sensor1_test_test
@@ -81,59 +83,59 @@ run_openvasd_cert_tar() {
             --openvasd-server-cert server.crt \
             --openvasd-server-key server.key \
             --openvasd-client-ca ca.crt
-        echo_task 'Test Openvasd Cert update'
+        echo_task 'Test openvasd cert tar update'
         openvas-deployment --update
-        echo_task 'Test Openvasd Cert run'
+        echo_task 'Test openvasd cert tar run'
         export BRIDGE_BACKENDS_SUBNET_IPV4='100.104.0.128/26'
         export BRIDGE_BACKENDS_SUBNET_IPV6='fd7a:91c3:4e82:3::/64'
         openvas-deployment --run --openvasd-port '1337'
         if ! ss -ltn | grep -q ':1337 '; then
-            echo_error 'Openvasd sensor setup port test failed!'
+            echo_error 'Error openvasd sensor setup port test failed!'
             exit 1
         fi
     popd > /dev/null
     echo_task 'Test add openvasd to gvmd'
     gvmd_add_openvasd_host_to_etc_hosts 'sensor1.test.test' '100.104.0.128'
+    openvas-deployment --add-openvasd --cn-openvasd sensor1.test.test --openvasd-port 1337
+    echo_task 'Cleanup Openvasd cert tar'
     pushd sensor1_test_test > /dev/null || exit
-        clean
-        list
+        clean 'Openvasd cert tar'
+        list 'Openvasd cert tar'
     popd > /dev/null
-    rm -rf sensor1_test_test
-    rm -f sensor1_test_test.tar
 }
 
 run_openvasd_tar() {
-    echo_task 'Test Openvasd Gen'
+    echo_task 'Test openvasd gen'
     openvas-deployment --create-openvasd-certs --cn-openvasd sensor2.test.test
-    echo_task 'Test Openvasd Tar'
+    echo_task 'Test openvasd tar'
     openvas-deployment --create-openvasd-tar --cn-openvasd sensor2.test.test
     ls
     if ! [ -f 'sensor2-test-test.tar.gz' ]; then
-        echo_error 'Openvasd tar gen failed!'
+        echo_error 'Error openvasd tar gen failed!'
         exit 1
     fi
     mkdir sensor2_test_test
     pushd sensor2_test_test > /dev/null || exit
-        echo_task 'Test Openvasd extract'
+        echo_task 'Test openvasd extract'
         tar xzvf ../sensor2-test-test.tar.gz
-        echo_task 'Test Openvasd run'
+        echo_task 'Test openvasd run'
         export BRIDGE_BACKENDS_SUBNET_IPV4='100.104.0.192/26'
         export BRIDGE_BACKENDS_SUBNET_IPV6='fd7a:91c3:4e82:4::/64'
         openvas-deployment --run --openvasd-port '2337'
         if ! ss -ltn | grep -q ':2337 '; then
-            echo_error 'Openvasd sensor setup port test failed!'
+            echo_error 'Error openvasd sensor setup port test failed!'
             exit 1
         fi
 
     popd > /dev/null
     echo_task 'Test add openvasd to gvmd'
     gvmd_add_openvasd_host_to_etc_hosts 'sensor2.test.test' '100.104.0.192'
+    openvas-deployment --add-openvasd --cn-openvasd sensor2.test.test --openvasd-port 2337
+    echo_task 'Cleanup Openvasd tar'
     pushd sensor2_test_test > /dev/null || exit
-        clean
-        list
+        clean 'Openvasd tar'
+        list 'Openvasd tar'
     popd > /dev/null
-    rm -rf sensor2_test_test
-    rm -f sensor2_test_test.tar
 }
 
 check_req
