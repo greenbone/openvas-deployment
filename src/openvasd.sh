5e91433a14de1c621a0eb6a59609ac2abdfa14fb
Pascal Holthaus <94793111+pascalholthaus@users.noreply.github.com>
2026-09-14 10:31:21 +0200

    Change: update help text

--- src/openvasd.sh
+++ src/openvasd.sh
@@ -186,7 +186,7 @@ Create an OpenVASD deployment archive for an external sensor:
 
   ${0} --create-openvasd-tar --cn-openvasd ${openvasd_cn}
 
-Include Docker images in the archive (no --init-openvasd required):
+Include Docker images in the archive (no --init-openvasd-tar required):
 
   ${0} --create-openvasd-tar \\
     --cn-openvasd ${openvasd_cn} \\
@@ -199,12 +199,10 @@ Deploy the sensor from an archive:
 2. Extract the archive.
 3. Initialize and start the sensor:
 
-  ${0} --init-openvasd
+  ${0} --init-openvasd-tar
   ${0} --run
 
-
-Load packaged Docker images before starting the sensor
-(no --init-openvasd required):
+Or load Docker images from the archive (no --init-openvasd-tar required):
 
   ${0} --run --openvasd-load-images-from-tar
 
